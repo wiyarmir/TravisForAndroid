c515694f9d9d9eb6391ddbf3f7939a0dd0189900
Guillermo Orellana <guillermo.orellana@corp.badoo.com>
2016-11-09 11:02:31 +0000

    attempt to run x86 emulators in circleci

--- ci.sh
+++ ci.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # This will: compile the project, run lint, run tests under JVM, package apk, check the code quality and run tests on the device/emulator.
-./gradlew clean build checkStyle findbugs pmd connectedCheck --stacktrace --console=plain -PdisablePreDex
+./gradlew clean build checkStyle checkStyleHtml findbugs pmd connectedCheck --stacktrace --console=plain -PdisablePreDex
