f26a6d7e82a0674dbade2063d1f569cc98ac2830
Guillermo Orellana <wiyarmir@gmail.com>
2016-11-09 00:35:51 +0000

    I hate robolectric lets get rid of it also why the heck was I testing models

--- app/src/main/java/es/guillermoorellana/travisforandroid/ui/presenter/ReposPresenter.java
+++ app/src/main/java/es/guillermoorellana/travisforandroid/ui/presenter/ReposPresenter.java
@@ -19,6 +19,7 @@ package es.guillermoorellana.travisforandroid.ui.presenter;
 import android.content.Context;
 import android.database.Cursor;
 import android.support.annotation.NonNull;
+import android.support.annotation.VisibleForTesting;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
 
@@ -29,6 +30,7 @@ import es.guillermoorellana.travisforandroid.data.TravisDatabase;
 import es.guillermoorellana.travisforandroid.model.Repo_Table;
 import es.guillermoorellana.travisforandroid.mvp.BasePresenter;
 import es.guillermoorellana.travisforandroid.ui.view.ReposView;
+import rx.Scheduler;
 import rx.Single;
 import rx.Subscription;
 import rx.android.schedulers.AndroidSchedulers;
@@ -37,12 +39,21 @@ import timber.log.Timber;
 
 public class ReposPresenter extends BasePresenter<ReposView> {
 
-    @NonNull private final Repository repository;
+    @NonNull
+    private final Repository repository;
     private Subscription subscription;
+    private Scheduler scheduler;
 
     @Inject
     public ReposPresenter(@NonNull Repository repository) {
+        this(repository, AndroidSchedulers.mainThread());
+    }
+
+    @VisibleForTesting
+    ReposPresenter(@NonNull Repository repository, @NonNull Scheduler scheduler) {
+
         this.repository = repository;
+        this.scheduler = scheduler;
     }
 
     public void reloadData() {
@@ -56,7 +67,7 @@ public class ReposPresenter extends BasePresenter<ReposView> {
     private void subscribe(Single<Integer> repos) {
         unsubscribe();
         subscription = repos.subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
+                .observeOn(scheduler)
                 .subscribe((n) -> {
                 }, (error) -> {
                     Timber.e(error, "error reloading data");
--- app/src/unitTests/java/es/guillermoorellana/travisforandroid/ui/presenter/ReposPresenterTest.java
+++ app/src/unitTests/java/es/guillermoorellana/travisforandroid/ui/presenter/ReposPresenterTest.java
@@ -19,17 +19,18 @@ package es.guillermoorellana.travisforandroid.ui.presenter;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
-import es.guillermoorellana.travisforandroid.TravisDroidRobolectricTestRunner;
 import es.guillermoorellana.travisforandroid.data.Repository;
 import es.guillermoorellana.travisforandroid.ui.view.ReposView;
 import rx.Single;
+import rx.schedulers.Schedulers;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-@RunWith(TravisDroidRobolectricTestRunner.class)
+@RunWith(JUnit4.class)
 public class ReposPresenterTest {
 
     private ReposPresenter presenter;
@@ -38,7 +39,7 @@ public class ReposPresenterTest {
     @Before
     public void setUp() {
         repository = mock(Repository.class);
-        presenter = new ReposPresenter(repository);
+        presenter = new ReposPresenter(repository, Schedulers.immediate());
     }
 
     @Test
